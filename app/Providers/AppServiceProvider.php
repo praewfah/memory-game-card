2eebfce837ce04f6e8bacf247c38f95a8147a440
Praew <preawfah@hotmail.com>
2021-02-08 00:04:29 +0700

    Public

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Add the following line
+        Schema::defaultStringLength(191);
     }
 
     /**
